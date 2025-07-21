f0d304b75c29e523a92702d04a9efd1b8c3468dc
karthika-35 <karthikapjoshi@gmail.com>
2025-07-21 21:38:44 +0530

    signin completed

--- app.js
+++ app.js
@@ -13,6 +13,44 @@ app.use(Cors())
 
 
 Mongoose.connect("mongodb+srv://karthika_35:karthika35@cluster0.aa1yfln.mongodb.net/BlogappDb?retryWrites=true&w=majority&appName=Cluster0")
+
+//Sign In
+
+app.post("/signIn", async (req, res) => {
+
+    let input = req.body
+    let result = userModel.find({ email: req.body.email }).then(
+        (items) => {
+            if (items.length > 0) {
+
+                const passwordValidator = Bcrypt.compareSync(req.body.password, items[0].password)
+                if (passwordValidator) {
+                    jwt.sign({ email: req.body.email }, "blogApp", { expiresIn: "1d" },
+                        (error, token) => {
+                            if (error) {
+                                res.json({ "status": "error", "errorMessage": error })
+                            } else {
+                                res.json({ "status": "success", "token": token, "UserId": items[0]._id })
+                            }
+                        })
+                } else {
+                    res.json({ "Status": "Incorrect Password" })
+                }
+
+
+            } else {
+                res.json({ "Status": "Invalid email Id" })
+            }
+        }
+
+
+    ).catch()
+})
+
+
+
+
+//Sign Up
 app.post("/signup", async (req, res) => {
 
     let input = req.body
